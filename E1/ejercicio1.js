d52c50ba13ded97c639326bdc977aee010c35108
kitosLord <gg6109890@gmail.com>
2024-07-11 22:08:31 -0600

    Update ejercicio1.js

--- E1/ejercicio1.js
+++ E1/ejercicio1.js
@@ -1,9 +1,12 @@
 document.getElementById('boton1').addEventListener( 'click', function(){
 
-    let numero = document.getElementById('input-1').value
+   let numero = document.getElementById('input-1').value
 
-    if (numero >= 18  ){
+    if (numero >= 18 &&  numero < 99 ){
         alert('eres mayor de edad ya puedes conducir :)')
+    }
+    else if (numero > 99){
+        alert('eres muy viejo ya no puedes')
     }
     else if (numero <= 0){
         alert( 'no se aceptan numeros negativos o 0')
@@ -11,5 +14,6 @@ document.getElementById('boton1').addEventListener( 'click', function(){
     else {
         alert( 'no puedes conducir eres menor aún')
     }
+
 
 })
